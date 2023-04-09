51cd1b16b71fd1cd7fc72e92a3ad074086817d4f
callme-devil <pransermi@gmail.com>
2023-04-09 19:58:21 +0430

    feat: Added axios to send request

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import './App.css';
+import axios from 'axios'
 import SearchForm from './components/SearchForm'
 
 class App extends React.Component {
 
     onSearchFormSubmit = (value) =>{
         console.log('test')
+        axios.get("https://api.unsplash.com/search/photos" , {
+            headers: {
+                Authorization: "Client-ID  ???????"
+            },
+            params:{
+                query: value
+            }
+        }).then((result =>{
+            console.log(result.data)
+        }))
     }
 
     render() {
