487adaddc86a5c7baa345a1284cb8f0b5484323b
callme-devil <pransermi@gmail.com>
2023-04-09 20:19:58 +0430

    feat: ImageList

--- src/components/ImagesList.jsx
+++ src/components/ImagesList.jsx
@@ -4,7 +4,17 @@ const ImagesList = (props) => {
     return (
         <div>
             <div className="result-count-container">Result: <span>{props.Data_Image.total ? props.Data_Image.total : 0}</span></div>
-           
+            {(props.Data_Image.total && props.Data_Image.results) && (
+                <div className="image-list">
+                  {props.Data_Image.results.map((image) =>{
+                    return (
+                        <div className="image-card">
+                            <img src={image.urls.full} alt={image.alt_description} />
+                        </div>
+                    )
+                  })}
+                </div>
+            )}
         </div>
     )
 };
